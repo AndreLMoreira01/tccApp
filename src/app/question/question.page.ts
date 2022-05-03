c9b3c9f40e972a1353c1209c1c0ab718b7cfb98c
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-05-03 08:24:23 -0300

    Finalização do Css e ts

--- src/app/question/question.page.ts
+++ src/app/question/question.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Question, QuestionAnswer } from '../models/question';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionPage implements OnInit {
 
+  questions: Question[] = [
+
+    {
+      title: 'Pergunta 1',
+      answers: [
+        {description: 'Resposta 1 P1', isRight: false},
+        {description: 'Resposta 2 P1', isRight: true},
+        {description: 'Resposta 3 P1', isRight: false},
+        {description: 'Resposta 4 P1', isRight: false},
+      ]
+},
+
+{
+  title: 'Pergunta 2',
+  answers: [
+    {description: 'Resposta 1 P2', isRight: true},
+    {description: 'Resposta 2 P2', isRight: false},
+    {description: 'Resposta 3 P2', isRight: false},
+    {description: 'Resposta 4 P2', isRight: false},
+  ]
+},
+
+{
+  title: 'Pergunta 3',
+  answers: [
+    {description: 'Resposta 1 P3', isRight: false},
+    {description: 'Resposta 2 P3', isRight: false},
+    {description: 'Resposta 3 P3', isRight: false},
+    {description: 'Resposta 4 P3', isRight: true},
+  ]
+}
+
+  ];
+
+  curQuesion: Question;
+  questionIndex: number = 0;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.curQuesion = this.questions[this.questionIndex];
   }
 
+  doAnswer(answer: QuestionAnswer) {
+    if(answer.isRight){
+      this.questionIndex++;
+      this.curQuesion = this.questions[this.questionIndex];
+    }
+  }
 }
