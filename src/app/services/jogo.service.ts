be6a4087313f6493d3953aaabf94ec99513e7e31
LuizDavilaa <luiz.davila@etec.sp.gov.br>
2022-09-04 00:35:06 -0300

    Personalização - tela inicial

--- src/app/services/jogo.service.ts
+++ src/app/services/jogo.service.ts
@@ -1,25 +1,22 @@
-import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { apiUrl } from '../app.api';
+import { Observable } from 'rxjs';
 import { IJogo } from '../models/IJogo';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class JogoService{
 
-private requestMapping = '/jogos';
+  private requestMapping = '/jogos';
 
-constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-//por id
-buscarId(idJogo: number): Observable<IJogo>{
-return this.http.get<IJogo>(`${apiUrl + this.requestMapping}/${idJogo}`);
-}
-
-buscarJogo(nome: string, descricao: string): Observable<IJogo[]>{
-  return this.http.get<IJogo[]>(`${apiUrl + this.requestMapping}?nome=${nome}&descricao=${descricao}`);
-  }
+  buscarId(idJogo: number): Observable<IJogo>{
+    return this.http.get<IJogo>(`${apiUrl + this.requestMapping}/${idJogo}`);
+    }
 
 }
