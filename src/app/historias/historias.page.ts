e3cf5dc87a914101375c2331cc0adb94eefd4832
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-10-04 08:46:46 -0300

    Fazendo as Historias E o Seletor

--- src/app/historias/historias.page.ts
+++ src/app/historias/historias.page.ts
@@ -9,14 +9,46 @@ import { IHistoria } from '../models/IHistoria.model';
 })
 export class HistoriasPage implements OnInit {
 
-  historias: IHistoria[] = [];
+  currentHistoria = undefined;
 
-  constructor(private historiasService: HistoriaService ) { }
+  historias = [
+    { id: 1, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 2, nome: 'indiana', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 3, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 4, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 5, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 6, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 7, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 8, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 9, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 10, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 11, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+    { id: 12, nome: 'Coraline', autor: 'autor', descricao: 'duduxo', tipo: 'eu nao sei', conquista: 'tipo'},
+  ];
+
+  buscarHistoria(index: number, itemObject: any) {
+    return itemObject.id;
+  }
+  compareWith(o1, o2) {
+    if (!o1 || !o2) {
+      return o1 === o2;
+    }
+
+    if (Array.isArray(o2)) {
+      return o2.some((o) => o.id === o1.id);
+    }
+
+    return o1.id === o2.id;
+  }
+
+  handleChange(ev) {
+    this.currentHistoria = ev.target.value;
+  }
 
   ngOnInit(): void {
-    this.carregarHistorias();
+
   }
-  
+
   carregarHistorias(): void{
 
   }
