90a984d43230e088b1b425a794af29cf9d457c81
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-10-16 14:25:45 -0300

    tirando o alert

--- src/app/teste/teste.page.ts
+++ src/app/teste/teste.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AlertController } from '@ionic/angular';
 import { IJogo } from '../models/IJogo';
 import { JogoService } from '../services/jogo.service';
 
