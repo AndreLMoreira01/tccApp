47b212cc4886b5111a17d9f113570075c3be0631
André L Moreira <andre.moreira12@etec.sp.gov.br>
2022-10-18 19:59:39 -0300

    Buscando historias pelo id

--- src/app/services/historia.service.ts
+++ src/app/services/historia.service.ts
@@ -20,5 +20,11 @@ return this.http.get<IHistoria[]>(`${this.url}/todos`).pipe(
   (retorno => retorno));
 }
 
+buscarHistoiraPeloId(idHistoria: number): Observable<IHistoria> {
+  return this.http.get<IHistoria>(`${this.url}/${idHistoria}`).pipe(
+    (retorno => retorno)
+  );
+}
+
 };
 
