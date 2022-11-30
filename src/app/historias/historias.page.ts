c177a7ee6f57a1fb154006f39b22fc988be5e9f2
Luiz D'Avila <83254624+LuizDavilaa@users.noreply.github.com>
2022-11-30 12:26:42 -0300

    arrumando conquista Marte

--- src/app/historias/historias.page.ts
+++ src/app/historias/historias.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertMarsComponent } from '../alerts/alert-mars/alert-mars.component';
 import { IHistoria } from '../models/IHistoria.model';
 import { HistoriaService } from '../services/historia.service';
 
@@ -11,6 +12,7 @@ import { HistoriaService } from '../services/historia.service';
 export class HistoriasPage implements OnInit {
 
   historia: IHistoria;
+  modalController: any;
 
   constructor(private historiaService: HistoriaService,
     private activatedRoute: ActivatedRoute) { }
@@ -28,6 +30,22 @@ export class HistoriasPage implements OnInit {
       this.historia = retorno;
     });
   }
+
+
+  unlockMars(){
+    this.showModal('O planeta foi desbloqueado');
+  }
+
+  async showModal( msg ){
+
+    const modal = await this.modalController.create({
+      component: AlertMarsComponent,
+      componentProps: { message: msg },
+      cssClass: 'alert-mars'
+    });
+
+      await modal.present();
+    }
 
 
 }
